bc06b8139e8f018f3ce7911281b939bdf2bec5c5
evicsss <evilavictoria@hotmail.com>
2024-10-23 23:01:12 -0300

    Update backend

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 async function conectaBancoDeDados() {
     try{
     console.log('Conexão com o banco de dados começou')
     
-    await mongoose.connect('mongodb+srv://evilavictoria:GcDuH6nFHdun2SRW@users.0w6on.mongodb.net/?retryWrites=true&w=majority&appName=Users')
+    await mongoose.connect(process.env.MONGO_URL)
 
     console.log('Parabens! Banco de dados feito com sucesso')
 } catch(erro) {
